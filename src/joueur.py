02cd30e6f719e2151e7d0eff3bd819d26dd7581a
Axel <axelcastelot@gmail.com>
2026-04-10 22:22:56 +0200

    Ajout de la méthode pour ajouter une carte à un joueur et tests

--- src/joueur.py
+++ src/joueur.py
@@ -2,3 +2,6 @@ class Joueur:
     def __init__(self, nom):
         self.nom = nom
         self.main = []
+        
+    def ajouter_carte(self, carte):
+        self.main.append(carte)
--- tests/test_joueur.py
+++ tests/test_joueur.py
@@ -1,6 +1,16 @@
 from src.joueur import Joueur
+from src.carte import Carte
 
 def test_creation_joueur():
     joueur = Joueur("Toto")
     assert joueur.nom == "Toto"
     assert joueur.main == []
+
+def test_ajouter_carte():
+    joueur = Joueur("Toto")
+    joueur.ajouter_carte(Carte(6, "Pique"))
+    assert len(joueur.main) == 1
+    
+    joueur = Joueur("Toto")
+    joueur.ajouter_carte(Carte("Valet", "Pique"))
+    assert len(joueur.main) == 1
